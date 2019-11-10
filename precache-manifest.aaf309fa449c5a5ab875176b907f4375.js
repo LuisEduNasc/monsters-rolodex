43c6d9875742bdeb48a03981ca6dd05775a3b234
LuisEduNasc <luisedu_nascimento@hotmail.com>
2019-11-10 19:45:39 -0300

    Updates

---
+++ precache-manifest.aaf309fa449c5a5ab875176b907f4375.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0947b292c0f002499357b2a2fba62aa7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8a5a71f89bc1d3f18ea4",
+    "url": "/monsters-rolodex/static/css/main.852b174f.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "8a5a71f89bc1d3f18ea4",
+    "url": "/monsters-rolodex/static/js/main.e563f6b6.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
